7a213d99c2d8e89f2cbd070bfc1ac40eb4978be8
AmriteshSingh0 <amriteshsingh20030@gmail.com>
2025-07-01 23:18:24 +0530

    ready for production

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
  
-const BASE_URL =  'http://localhost:5000/api';
+const BASE_URL =import.meta.env.VITE_API_URL;
+
 
 export const uploadPost = async(postData)=>{
     try{
